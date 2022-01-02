1496195117e0ae1e338b726719ed1ed368130984
pinar <pinarunalcan50@gmail.com>
2022-01-02 22:58:10 +0100

    proje updated

--- cypress/integration/Cypress_Works/Day07/Debugger.spec.js
+++ cypress/integration/Cypress_Works/Day07/Debugger.spec.js
@@ -16,21 +16,25 @@ context("Hata ayiklama yontemleri",()=>{
       //  cy.xpath(submitInputXpath).click()
   //
         cy.visit("https://test.iyikisordun.com/index.php")
-        cy.xpath("//label[@for='todoCheck']").click().debug()  //debug=hata ayiklamasi
+        cy.xpath("//label[@for='todoCheck1']").click()
+      //  cy.get(".todo-list.ui-sortable > li:nth-of-type(1)").should("have.class","done")
         cy.xpath("(//li[@class='done'])[1]").should("have.class","done")
-        //cy.pause()
-      
+        cy.pause()
+        //3.secenekteki yaziyi ekrana yazdiralim
+        cy.xpath("(//span[.='Let theme shine like a star'])[1]").invoke("text").then((str)=>{
 
-        cy.xpath("(//span[.='Let theme shine like a star'])[2]").invoke("text").then((str)=>{
-          str=str.trim()
-         // cy.log(str) .debug() //inspect bolumu acik olmali
-        //  debugger
-      
-        }
-        )
-      //  debugger
-      
+        str=str.trim()
+        cy.log(str).debug()   //debug pause isleminin benzeridir buraya geldigi zaman normalde durmaz
+        ///inspect bolumu acikken log islemi sonrasi ekran durur/yani debug'a gelince durur
+        //bu hata ayiklama yontemidir
+      //  debugger   //! buraya yazilinca sadce yazilan yerde durur
+       
     })
-  
-    
+       // debugger    //* burdaki debugger proje casismadan ilk olarak okundugu icin bastan itirbaren durur
+        //sebebi cypress java script komutlarini kedni komutlarindan once calsitirir
+        //bunun onune gecebilmek iicn debugger 'i then yapisiniin icine koymak gerekiyor
+        cy.xpath("//label[@for='todoCheck4']").click()
+      
+        
+    })
 })
