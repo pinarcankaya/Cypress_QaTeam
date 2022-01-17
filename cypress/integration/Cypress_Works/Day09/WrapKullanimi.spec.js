a940be6bdfd75121b3055cf0346ef233d2bdd7c2
mithtc <mithtc06@gmail.com>
2022-01-17 21:46:42 +0100

    duzenlenmis Day10

--- cypress/integration/Cypress_Works/Day09/WrapKullanimi.spec.js
+++ cypress/integration/Cypress_Works/Day09/WrapKullanimi.spec.js
@@ -1,6 +1,7 @@
 ///<reference types ="Cypress"/>
 
-//wrap kullanilmadan , then fonksiyonu icindeki bir degiskeni baska bir yere tasiyamiyoruz,once Sar,sonra Tasi  
+//wrap kullanilmadan , then fonksiyonu icindeki bir degiskeni baska bir yere tasiyamiyoruz,
+//once Sar,sonra Tasi  
 context("Then Yapisi" ,()=>{
 
     
@@ -8,28 +9,31 @@ context("Then Yapisi" ,()=>{
         cy.visit("http://test.iyikisordun.com/pages/forms/general.html")
 
       let mail
-        
-       cy.get("input#exampleInputEmail1").invoke("val").then((emailAdress)=>{
-            
-           mail=emailAdress
+        //invoke getAttribute gibi calisiyor.invoke('val') ve invoke('text')
+        //Diger Attribute'ler bu sekilde=>      invoke('attr','type')
+       cy.get("input#exampleInputEmail1").invoke('val').then((emailAdress)=>{ 
+            mail=emailAdress
             cy.log(emailAdress)
-           cy.wrap(emailAdress).as("newEmail")
+            
+           cy.wrap(emailAdress).as("newEmail") //!!Wrap then fonk.icinde olusturulmalidir.
+                                               //wrap'in icine then 'deki isim TIRNAKSIZ yazilmalidir.
+                                               //Wrap'tan sonra MUTLAKA as ile isim verilmelidir.
         })
 
-    
-       // cy.getText_Product_name_css("input#exampleInputEmail1")
-        console.log(mail)
-
-        //cy.pause()
+       // cy.log(mail)//Degiskeni fonksiyonun disinda tanimlayinca da log bos kaliyor.
       
-        // cy.get("@newEmail").then((email)=>{
-        //     cy.log(email)
-        //  })
-        // cy.visit("http://test.iyikisordun.com/pages/forms/validation.html")
-        // cy.pause()
-        // cy.get("@newEmail").then((email)=>{
-        //     cy.get("input#exampleInputEmail1").type(email)
-        // }) 
+         
+         //as ile olusturulan isim @kullanilarak cy.get ile cagirilir,then fonk.olusturulur ve devam...
+         cy.get("@newEmail").then((email)=>{
+             cy.log(email)
+         })//Burayi  sadece yazdirmak icin kullandik.
+        
+        
+        cy.visit("http://test.iyikisordun.com/pages/forms/validation.html")
+        cy.pause()
+        cy.get("@newEmail").then((email)=>{
+            cy.get("input#exampleInputEmail1").type(email)
+       }) 
 
        
         
